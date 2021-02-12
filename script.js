da75426a9a3c3183750261d1ec8284864fd0efab
miswhiramon <yh7500u@gmail.com>
2021-02-13 04:06:11 +0900

    commit

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const fileup = (e) => {
             const ctx = canvas.getContext('2d');
             canvas.width = imgReader.width;
             canvas.height = imgReader.height;
-            ctx.drawImage(imgReader,0,70,imgReader.width,imgReader.height+70);
+            ctx.drawImage(imgReader,0,0,imgReader.width,imgReader.height+0);
             console.log(imgReader.width)
             console.log(imgReader.height)
             img.src = canvas.toDataURL(imgType);
