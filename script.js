a48f90da7e23131d8c1aacc1f583a461c3c6603b
miswhiramon <yh7500u@gmail.com>
2021-02-16 04:52:03 +0900

    commit

--- script.js
+++ script.js
@@ -224,8 +224,11 @@ const app = async () => {
     //prediction=prediction.unstack(0)[0]
     let offset_mul = tf.scalar(127.5);
     let offset_add = tf.scalar(1);
+    console.log("A");
     prediction=prediction.unstack(0)[0].add(offset_add).mul(offset_mul);
+    console.log("B");
     var image = prediction.clipByValue(0,255).toInt();
+    console.log("C");
 
     var temp = document.createElement("canvas");
     await tf.browser.toPixels(image,temp);
