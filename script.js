45500d4547ec842d98b3b81a16f8b3ff395c1cc8
miswhiramon <yh7500u@gmail.com>
2021-02-16 02:11:11 +0900

    commit

--- script.js
+++ script.js
@@ -145,6 +145,7 @@ const fileup = (e) => {
             console.log(imgReader.width)
             console.log(imgReader.height)
             img.src = canvas.toDataURL(canvas);
+            progress_bar.setAttribute("style", "width:10%");
             progress_bar.style.width="width:10%";
         }
         imgReader.src = reader.result;
