ec1367b08c2ad5df79cb18d73878865ab3ba9afc
miswhiramon <yh7500u@gmail.com>
2021-02-16 17:49:45 +0900

    commit

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ var progress_bar = document.getElementById('progress_bar');
 
 var canvas = document.createElement('canvas');
 
-tf.setBackend('cpu');
+tf.setBackend('gpu');
 
 const fileup = (e) => {
     console.log(e)
